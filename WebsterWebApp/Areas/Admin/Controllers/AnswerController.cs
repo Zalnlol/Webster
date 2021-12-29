0c1426125e6fc68bdb1a3cd86a5e0de2c648be8e
vuongpham99 <86349987+vuongpham99@users.noreply.github.com>
2021-12-29 18:06:54 +0700

    up

--- WebsterWebApp/Areas/Admin/Controllers/AnswerController.cs
+++ WebsterWebApp/Areas/Admin/Controllers/AnswerController.cs
@@ -8,6 +8,7 @@ using WebsterWebApp.Areas.Admin.Models;
 
 namespace WebsterWebApp.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AnswerController : Controller
     {
         private readonly Data.DatabaseContext _db;
@@ -15,6 +16,11 @@ namespace WebsterWebApp.Areas.Admin.Controllers
         public AnswerController(Data.DatabaseContext db)
         {
             this._db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Answers.ToList());
         }
     }
 }
