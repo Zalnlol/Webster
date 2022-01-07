b59fd74f07774254224ef9e0a0389b372106c35a
Lê Hữu Tâm <72965112+thiendzlathe@users.noreply.github.com>
2022-01-07 14:57:50 +0700

    update exam

---
+++ WebsterWebApp/Areas/Admin/Models/ExamType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebsterWebApp.Areas.Admin.Models
+{
+    [Table("tb_ExamType")]
+    public class ExamType
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ExamTypeId { get; set; }
+        public int ExamId { get; set; }
+        public int QuestionId { get; set; }
+        public virtual Exam Exam { get; set; }
+        public virtual Question Question { get; set; }
+    }
+}
--- WebsterWebApp/Data/DatabaseContext.cs
+++ WebsterWebApp/Data/DatabaseContext.cs
@@ -17,5 +17,8 @@ namespace WebsterWebApp.Data
         public virtual DbSet<Question> Questions { get; set; }
 
         public virtual DbSet<Answer> Answers { get; set; }
+        public virtual DbSet<Exam> Exams { get; set; }
+        public virtual DbSet<ExamType> ExamTypes { get; set; }
+
     }
 }
