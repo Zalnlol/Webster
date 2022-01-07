d8187cf16b59d1b2422ae14f55c667564000a5c8
Lê Hữu Tâm <72965112+thiendzlathe@users.noreply.github.com>
2022-01-07 15:22:45 +0700

    Update ExamController.cs

--- WebsterWebApp/Areas/Admin/Controllers/ExamController.cs
+++ WebsterWebApp/Areas/Admin/Controllers/ExamController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
