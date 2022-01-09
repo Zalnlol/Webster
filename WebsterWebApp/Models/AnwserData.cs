4d3205781e470953d1f9929f4cfa401bd38f1679
Lê Nguyễn Thành Nhân <74607205+lenhannhan123@users.noreply.github.com>
2022-01-09 21:40:22 +0700

    up

---
+++ WebsterWebApp/Models/AnwserData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsterWebApp.Models
+{
+    public class AnwserData
+    {
+        public int  AnwserId { get; set; }
+        public string AnwserName { get; set; }
+        public bool Ischoose { get; set; }
+
+    }
+}
---
+++ WebsterWebApp/Models/QuestionData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsterWebApp.Models
+{
+    public class QuestionData
+    {
+        public int QuesionID { get; set; }
+        public string QuestionTilte { get; set; }
+        public bool QuestionType { get; set; }
+
+        public List<Models.AnwserData> Anwser { get; set; } = new List<AnwserData>();
+
+    }
+}
